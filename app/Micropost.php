bd9a061e55bea1519098ec80f2b1e9bc25dde38c
ryoya <ayuoyruruhoo2@gmail.com>
2020-06-10 10:29:00 +0000

    follow,unfollow

--- app/Micropost.php
+++ app/Micropost.php
@@ -8,6 +8,9 @@ class Micropost extends Model
 {
     protected $fillable = ['content'];
     
+    /**
+     * この投稿を所有するユーザ。（ Userモデルとの関係を定義）
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
